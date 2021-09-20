4f4625bf9fcf51b307e5490d5d6073c139262355
caijingjing <1966226779@qq.com>
2021-09-20 11:26:28 +0800

    修改了way

--- 关键字.py
+++ 关键字.py
@@ -1,21 +1,20 @@
 import re
 
 from os import replace
+# D:\learnpython\test.cpp
 
-
-def output(way):    # 进行输处
-    if 1 <= way <= 5:
+def output(way):    # 进行输出
+    if 1 <= way <= 4:
         if 1 <= way:
             print('total num: ', totalnum)
         if 2 <= way:
             print("switch num: ", switchnum)
-        if 3 <= way:
             print('case num:', end='')
             for x in casenums:
                 print(' ', x, end='')
-        if 4 <= way:
+        if 3 <= way:
             print('\nif-else num: ', ifelsenum)
-        if 5 <= way:
+        if 4 <= way:
             print('if-elseif-else num: ', ifelseifelsenum)
     else:
         print('error!')
@@ -49,9 +48,14 @@ path = input("the path to the file: ")  # D:\learnpython\test.cpp
 way = int(input("grade: "))
 file = open(path)
 keyword = (
-    'auto', 'break', 'case', 'char', 'const', 'continue', 'default', 'do', 'double', 'else', 'enum', 'extern', 'float',
-    'for', 'goto', 'if', 'int', 'long', 'register', 'return', 'short', 'signed', 'sizeof', 'static', 'struct', 'switch',
-    'typedef', 'union', 'unsigned', 'void', 'volatile', 'while')    #将关键词放入tuple集合中
+    'auto', 'break', 'case', 'char',
+    'const', 'continue', 'default', 'do',
+    'double', 'else', 'enum', 'extern',
+    'float','for', 'goto', 'if',
+    'int', 'long','register', 'return',
+    'short', 'signed', 'sizeof', 'static',
+    'struct', 'switch', 'typedef', 'union',
+    'unsigned', 'void', 'volatile', 'while')    #将关键词放入tuple集合中
 totalnum = 0             #关键词总数
 switchnum = 0            #Switch总数
 casenums = [0]           #将每次Switch下的case总数放入list集合中
@@ -65,41 +69,40 @@ for line in file.readlines():       #遍历file中的每行
     totalnum += row_totalnum(line)                      #累加每行关键词的数量
     if way >= 2:
         switchnum += row_switchnum(line)                #累加每行Switch的数量
+        if row_switchnum(line) != 0:  # 遇到Switch,将之前的casenum添加到casenums集合中,然后将casenum清0
+            if casenum != 0:
+                if casenums[-1] == 0:
+                    casenums.pop(-1)
+                casenums.append(casenum)
+                casenum = 0
+            else:
+                pass
+        casenum += row_casenum(line)
         if way >= 3:
-            if row_switchnum(line) != 0:                #遇到Switch,将之前的casenum添加到casenums集合中,然后将casenum清0
-                if casenum != 0:
-                    if casenums[-1] == 0:
-                        casenums.pop(-1)
-                    casenums.append(casenum)
-                    casenum = 0
-                else:
-                    pass
-            casenum += row_casenum(line)
-            if way >= 4:
-                for word in line:
-                    elseifflag = 0
-                    if word == 'if':
-                        flag = 1
-                        for x in line:
-                            if x == 'else':
-                                flag = 0
-                                elseifflag = 1
-                                break
-                    if elseifflag == 1:
-                        continue
-                    if word == 'elif':
-                        flag = 0
-                        continue
-                    if word == 'else':
-                        for y in line:
-                            if y == 'if':
-                                break
+            for word in line:
+                elseifflag = 0
+                if word == 'if':
+                    flag = 1
+                    for x in line:
+                        if x == 'else':
+                            flag = 0
+                            elseifflag = 1
+                            break
+                if elseifflag == 1:
+                    continue
+                if word == 'elif':
+                    flag = 0
+                    continue
+                if word == 'else':
+                    for y in line:
                         if y == 'if':
-                            continue
-                        if flag == 1:
-                            ifelsenum += 1
-                        else:
-                            ifelseifelsenum += 1
+                            break
+                    if y == 'if':
+                        continue
+                    if flag == 1:
+                        ifelsenum += 1
+                    else:
+                        ifelseifelsenum += 1
 
 if casenum != 0:        #将最后一次Switch中的case数加到casenums集合中
     casenums.append(casenum)
